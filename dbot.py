8088d46693d64088c9f684d8d11fffee089d2233
ahmed Elbendary <ahmed.elbendary@ocucon.com>
2021-06-06 13:42:28 +0200

    27th

--- dbot.py
+++ dbot.py
@@ -1,38 +1,23 @@
-import discord
-import sys
-import os
-import logging
-logging.basicConfig(level=logging.DEBUG)
+import time
 
-logging.debug('Starting Application')
+import redis
+from flask import Flask
 
-if os.getenv('TOKEN') is not None:
-    token = os.environ.get('TOKEN')
-else:
-    token = sys.argv[1]
-print(f'Set token as {token}')
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
-join_greeting = '''Welcome to the DevOps Journey Discord! We are a new community that embraces everything DevOps related.
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
-#Resources Channel has plenty of free learning resources.
-
-#Devops Channel to spark up any devops related questions or conversations.
-
-YouTube Channel: https://www.youtube.com/channel/UC4Snw5yrSDMXys31I18U3gg
-'''
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        if message.author == client.user: return
-        if message.content == '!stop': await client.logout()
-        #if message.content.startswith('!ping'): await message.channel.send('Pong!')
-    
-    async def on_member_join(self, member):
-        await member.send(join_greeting)
-
-client = MyClient()
-client.run(token)
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n Ahmedelbendary/Travis'.format(count)
